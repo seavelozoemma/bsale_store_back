c37b09779b9a4a76440b50f9f6c9c979580d146d
Sebastian Aguilera Velozo <sebastian.aguileraE@hotmail.com>
2022-09-27 22:29:23 -0300

    Configuración cross origins

---
+++ src/main/java/com/ms/product/config/WebConfig.java
@@ -0,0 +1,19 @@
+package com.ms.product.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Configuration
+@EnableWebMvc
+public class WebConfig extends WebMvcConfigurerAdapter {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("https://unrivaled-phoenix-548977.netlify.app/")
+                .allowedMethods("GET")
+                .allowCredentials(false).maxAge(3600);
+    }
+}
